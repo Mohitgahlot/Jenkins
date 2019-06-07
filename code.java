7f0c3c45c2406d10c376f4f4f11b068f59b2840a
Mohitgahlot <gahlot.mohit6@gmail.com>
2019-06-07 11:13:08 -0700

    new changes

--- code.java
+++ code.java
@@ -2,6 +2,7 @@ public class code{
     public static void main(String args[]){
         for(int i=0;i<10;i++){
             System.out.println("Hello "+i);
+            System.out.println("New Changes");
         }
     }
 }
